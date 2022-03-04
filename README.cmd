1559c6ac8d3a800e1324d85a8cfe2d4fd6b97700
Jacob Thomsen <jat0220@byu.edu>
2022-03-03 23:43:16 -0700

    Added README files

---
+++ README.cmd
@@ -0,0 +1,43 @@
+#PokeAPI OIT Application Coding Challenge
+
+Created by Jacob Thomsen, 2 March 2022.
+This project was created as a part of an application for the BYU OIT Student Developer job position.
+
+
+##Requirements / Program Functions
+
+Using Python, this program can:
+
+1.    Accept user input of a Pokemon type and return a list of Pokemon of the given type
+
+2.    Allow the user to enter a name of a Pokemon. The given name must be from the information returned in step 1
+
+3.    Return information for the entered Pokemon name including at least the games the Pokemon are in,
+           all the types of the Pokemon, the name of one of the Pokemon's moves,
+           and one other value of your choice related to the Pokemon.
+
+
+##Running the program
+
+If you have cloned the repository for this code or unzipped the project folder, 
+enter the following commands to run the .exe version of the python script 'main.py':
+
+`> cd venv/dist/main`
+
+`> ./main`
+
+The script should now be running and you can interact with the program.
+
+
+##Notes
+
+In order to utilize the Python language and its dependencies
+and make it accessible to any user, this program is bundled
+with Pyinstaller. This program packages the script its
+dependencies into a .exe file that does not require a local
+Python interpreter to run the script.
+
+
+
+Jacob Thomsen - March 2022
+
